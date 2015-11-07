8697c916f63f804fd5969b15bf5c73fd702e3663
DariuszUstrzycki <us3cki@tlen.pl>
2015-11-07 18:01:58 +0100

    words, syllables, sentences working

--- MOOCTextEditor/src/document/BasicDocument.java
+++ MOOCTextEditor/src/document/BasicDocument.java
@@ -91,7 +91,7 @@ public class BasicDocument extends Document
 			
 			int syllables = countSyllables(word);
 			
-			System.out.println("Word: " + word + "  " + syllables + " syllables");
+			System.out.println("Word: " + word + "-" + syllables + " syllables");
 			
 			totalSyllables += syllables;
 		}
@@ -105,10 +105,23 @@ public class BasicDocument extends Document
 	public static void main(String[] args)
 	{
 		
+		
+		 testCase(new BasicDocument("cOntiguous"), 3, 1, 1);
+			testCase(new BasicDocument("sleepY"), 2, 1, 1);
+			testCase(new BasicDocument("obvious"), 2, 1, 1);
+			testCase(new BasicDocument("toga"), 2, 1, 1);
+		 
+		testCase(new BasicDocument("soMe"), 1, 1, 1);
+		testCase(new BasicDocument("flo"), 1, 1, 1);
+		testCase(new BasicDocument("the"), 1, 1, 1);
+		testCase(new BasicDocument("thE"), 1, 1, 1);
+		testCase(new BasicDocument("cave"), 1, 1, 1);
+		testCase(new BasicDocument("Segue"), 2, 1, 1);
+		testCase(new BasicDocument("Sentence"), 2, 1, 1);
 		testCase(new BasicDocument("This is a test.  How many???  "
 		        + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
 				16, 13, 5);
-		/*testCase(new BasicDocument(""), 0, 0, 0);
+		testCase(new BasicDocument(""), 0, 0, 0);
 		testCase(new BasicDocument("sentence, with, lots, of, commas.!  "
 		        + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
 		testCase(new BasicDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
@@ -116,12 +129,11 @@ public class BasicDocument extends Document
 				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
 				+ "the correct amount of syllables (example, for example), "
 				+ "but most of them will."), 49, 33, 3);
-		testCase(new BasicDocument("Segue"), 2, 1, 1);
-		testCase(new BasicDocument("Sentence"), 2, 1, 1);
+		
 		testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
 		testCase(new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
 		         32, 15, 1);
-		*/
+		
 		
 	}
 	
